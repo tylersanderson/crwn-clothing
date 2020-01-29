4daeb7184691c75f9c60a5572616084346cc2f9c
tylersanderson <tylersanderson@hotmail.com>
2020-01-29 16:49:25 -0600

    implemented firebase utils, including ability to store authenticated user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyBHyKgvBvEESqZqctcwDscd5PtbNPoDHE0",
@@ -11,7 +11,33 @@ const config = {
   messagingSenderId: "296170122487",
   appId: "1:296170122487:web:7ed8fe2eb579055e96f7f2",
   measurementId: "G-D48L2BPGRY"
-}
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 firebase.initializeApp(config);
 
@@ -19,7 +45,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ promp: 'select_account' });
+provider.setCustomParameters({ promp: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
