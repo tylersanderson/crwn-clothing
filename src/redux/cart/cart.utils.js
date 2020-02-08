a953a6ec1fe2e5f68daca1185887de6471f27dc4
tylersanderson <tylersanderson@hotmail.com>
2020-02-08 07:46:04 -0600

    created cart-item component and connect cart dropdown with cart items reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,9 +6,10 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
-        ? { cartItem, quantity: cartItem.quantity + 1 }
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
   }
+
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
